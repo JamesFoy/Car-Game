d75f918aed721c683410e2efeef328769d7bc5c0
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-04-29 01:55:41 +0100

    the "AddForceBasedOnHealth" script amplifies collision impules based on damage already accumulated by a player car

---
+++ Assets/Scripts/CollisionMechanics/ForceAdder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceAdder : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.down * 100);
+        GetComponent<Rigidbody>().AddForce(Vector3.left * 100);
+    }
+}
