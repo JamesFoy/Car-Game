c41cd65856c21ce931f3ef79f3c7a7715cd27aa1
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-04-30 16:18:43 +0100

    damage now increases based on collision magnitude between two player cars. this has the effect of causing bigger knockbacks as damage is accumulated

---
+++ Assets/Scripts/CollisionMechanics/AddDamageBasedOnForce.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddDamageBasedOnForce : MonoBehaviour
+{
+    Rigidbody rb;
+    CarData car;
+    int frames;
+    float lastFrameTimeDeltaTime;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (car == null)
+        {
+            car = GetComponent<CarController>().carData;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (frames < 1)
+        {
+            frames = 1;
+            lastFrameTimeDeltaTime = Time.deltaTime;
+        }
+        frames--;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 originalForce = collision.impulse / lastFrameTimeDeltaTime;
+            float forceMagnitude = originalForce.magnitude;
+            float damageApplied = forceMagnitude * 0.1f;
+            car.health += damageApplied;
+            Debug.Log(damageApplied);
+        }
+    }
+}
--- Assets/Scripts/CollisionMechanics/AddForceBasedOnHealth.cs
+++ Assets/Scripts/CollisionMechanics/AddForceBasedOnHealth.cs
@@ -32,10 +32,13 @@ public class AddForceBasedOnHealth : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Vector3 originalForce = collision.impulse / lastFrameTimeDeltaTime;
-        Vector3 healthScaledForce = originalForce * car.health;
-        rb.AddForce(healthScaledForce);
-        DebugLogForces(collision, originalForce);
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 originalForce = collision.impulse / lastFrameTimeDeltaTime;
+            Vector3 healthScaledForce = originalForce * car.health;
+            rb.AddForce(healthScaledForce);
+            //DebugLogForces(collision, originalForce);
+        }
     }
     Vector3 GetVelocity()
     {
