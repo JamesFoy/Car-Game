4a5da823d758977fc0c48dec3937e0c5d7e271f6
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-05-06 03:28:02 +0100

    game object independent checkpoint adder and remover now available. a checkpoint should consists of a gameobject with just a box collider trigger, and a Checkpoint component. make sure there is one CheckpointManager somewhere in the scene.

---
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public List<GameObject> CurrentCars { get; } = new List<GameObject>();
+
+    private void Start()
+    {
+        if (CheckpointManager.Instance != null)
+        {
+            CheckpointManager.Instance.AddThisCheckpointToCheckpointList(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        CheckpointManager.Instance.RemoveThisCarFromAllCheckpoints(other.gameObject);
+        AddCarToThisCheckpoint(other.gameObject);
+    }
+    void AddCarToThisCheckpoint(GameObject car)
+    {
+        CurrentCars.Add(car);
+        Debug.Log(car.name + " added to " + gameObject.name);
+    }
+    /// <summary>
+    /// Removes provided GameObject from this checkpoint
+    /// </summary>
+    /// <param name="car"></param>
+    public void RemoveCarFromThisCheckpoint(GameObject car)
+    {
+        CurrentCars.Remove(car);
+        Debug.Log(car.name + " removed from checkpoint " + gameObject.name);
+    }
+}
---
+++ Assets/Scripts/Checkpoint/CheckpointManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    List<Checkpoint> checkpoints = new List<Checkpoint>();
+    public static CheckpointManager Instance;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    /// <summary>
+    /// Adds the checkpoint instance provided to the list of checkpoints in CheckpointManager
+    /// </summary>
+    /// <param name="checkpoint"></param>
+    public void AddThisCheckpointToCheckpointList(Checkpoint checkpoint)
+    {
+        if (!checkpoints.Contains(checkpoint))
+        {
+            checkpoints.Add(checkpoint);
+        }
+    }
+    /// <summary>
+    /// Removes the given GameObject from all checkpoints
+    /// </summary>
+    /// <param name="car"></param>
+    public void RemoveThisCarFromAllCheckpoints(GameObject car)
+    {
+        foreach (Checkpoint a in checkpoints)
+        {
+            if (a.CurrentCars.Contains(car))
+            {
+                a.RemoveCarFromThisCheckpoint(car);
+            }
+        }
+    }
+}
