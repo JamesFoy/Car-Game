085eeef0b9bdc19728d85bbf4da239166a73b84e
James Home <jamesfoy17>
2020-05-07 19:07:18 +0100

    Script Commenting
    All of the scripts that have a major impact on the ability system are now commented. The ability behaviours are still needed to be commented however!

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 
-//abstract allows functions with no implementation within it
-//script setups up basic inforamtion about the ability
+//This scriptable object is the base object for all other ability scriptable objects
 public abstract class Ability : ScriptableObject
 {
-    public string Name = "New Ability";
-    public Sprite Sprite;
-    public AudioClip Sound;
+    public string Name = "New Ability"; //Variable for the name
+    public Sprite Sprite; //Variable for a sprite
+    public AudioClip Sound; //Variable for a sound
 
-    public abstract void Initialize(GameObject obj);
-    public abstract void TriggerAbility(AbilityDeployModes.DeployStyle style);
+    public abstract void Initialize(GameObject obj); //Method that will be overriden for Initializing an ability to a specific weapon holders script (this is based on which ability needs which script)
+    public abstract void TriggerAbility(AbilityDeployModes.DeployStyle style); //Method that will be overriden for Triggering each ability based on a deploy style
 }
--- Assets/Scripts/Abilities/ProjectileAbility.cs
+++ Assets/Scripts/Abilities/ProjectileAbility.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script for creating a new projectile ability which inherits from the ability scriptable object
 [CreateAssetMenu (menuName = "Scriptable Objects/Abilities/Projectile Ability")]
 public class ProjectileAbility : Ability
 {
-    public bool useGravity;
-    public float projectileForce = 1;
-    public Rigidbody projectile;
+    public bool useGravity; //bool to check if the ability should have gravity effect it
 
-    private ProjectileShootTriggerable launcher;
+    public float projectileForce = 1; //Variable that will have its value changed in the editor on the scriptable object that is created
 
-    //script that provides a funciton implementation
+    public Rigidbody projectile; //Variable to assign a Prefab for the ability to use (This is the thing that will be shot!!)
+
+    private ProjectileShootTriggerable launcher; //Variable that will contain the ProjectileShootTriggerable script
+
+    //This is the override method for Initalizeing the ability (This takes a GameObject called obj.... this is where the WeaponHolders are passed in to access the correct script etc.)
     public override void Initialize(GameObject obj)
     {
-        //implementation of script
-        launcher = obj.GetComponent<ProjectileShootTriggerable>();
-        launcher.projectileForce = projectileForce;
-        launcher.projectile = projectile;
-        launcher.projectile.useGravity = useGravity;
+        launcher = obj.GetComponent<ProjectileShootTriggerable>();//Sets the launcher variable to the ProjectileShootTriggerable script on the passed gameObject
+
+        launcher.projectileForce = projectileForce; //This sets the launchers projectile force to the abilities projectile force variable
+
+        launcher.projectile = projectile; //This will set the launchers (WeaponHolder's ProjectileShootTriggerable) projectile to the projectile assigned to the scriptable object (Again this is the prefab that will be spawned when a ability is used)
+
+        launcher.projectile.useGravity = useGravity; //This sets the projectile's gravity bool to the abilities useGravity bool
     }
 
+    //This method is used to trigger the ability (For example, it takes in an attack style to deploy the correctly ability when used e.g. attack style will activate attack style ability)
     public override void TriggerAbility(AbilityDeployModes.DeployStyle style)
     {
         launcher.Launch();
