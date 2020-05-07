8157730a285fb739e9b4624c3a5a2f96c2fd744e
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-05-07 01:12:44 +0100

    generic object pooling now works, with an example pooler for rockets placed in

---
+++ Assets/Scripts/ObjectManagement/ObjectPooler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public T prefabToPool;
+    public List<T> poolOfAvailableInstances = new List<T>();
+    public List<T> poolOfUsedInstances = new List<T>();
+
+    private void ExpandPool()
+    {
+        T newObject = Instantiate(prefabToPool);
+        poolOfAvailableInstances.Add(newObject);
+    }
+    /// <summary>
+    /// Fetches an object from the requested pool, and sets it active.
+    /// </summary>
+    public T GetObjectFromPool()
+    {
+        if (poolOfAvailableInstances.Count <= 0)
+        {
+            ExpandPool();
+        }
+        T objectToActivate = poolOfAvailableInstances[poolOfAvailableInstances.Count - 1];
+        objectToActivate.gameObject.SetActive(true);
+
+        poolOfAvailableInstances.Remove(objectToActivate);
+        poolOfUsedInstances.Add(objectToActivate);
+        return objectToActivate;
+    }
+    public void ReturnObjectToPool(T objectToReset)
+    {
+        objectToReset.transform.parent = transform;
+        objectToReset.transform.localPosition = Vector3.zero;
+        objectToReset.gameObject.SetActive(false);
+
+        poolOfUsedInstances.Remove(objectToReset);
+        poolOfAvailableInstances.Add(objectToReset);
+    }
+}
---
+++ Assets/Scripts/ObjectManagement/RocketPooler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketPooler : ObjectPooler<RocketBehaviour>
+{
+    private void Start()
+    {
+        GameObject a = GetObjectFromPool().gameObject;
+
+        GameObject b = GetObjectFromPool().gameObject;
+        ReturnObjectToPool(b.GetComponent<RocketBehaviour>());
+    }
+}
