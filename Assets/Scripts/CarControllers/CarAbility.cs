a1d0aba7884a9e13714ed524e2de7bad2bf2e552
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-05-07 02:32:47 +0100

    car ability management factored out into CarAbility.cs. pickup event is set to work with car ability. since we have no test suite, i have no way of verifiying this works for all scenarios

---
+++ Assets/Scripts/CarControllers/CarAbility.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAbility : MonoBehaviour
+{
+    public AbilityCoolDown ability; //Reference to the UI ability (needs to be called when a button is pressed)
+    public GameObject abilityUIImage; //Reference to the game object that contains the ability UI (makes sure that it only works when activated, activation when picking up powerup occurs etc)
+
+    //Creating a list of possible abilites/powerups
+    public List<AbilityHolder> possibleAbilities;
+    public AbilityHolder abilityTypeChosen;
+    public PowerupAbility powerup1;
+    public PowerupAbility powerup2;
+    public ProjectileAbility projectile1;
+    public ProjectileAbility projectile2;
+
+
+    private void Start()
+    {
+        abilityUIImage.SetActive(false);
+    }
+
+    //THIS METHOD IS CALLED USEING THE PICKUP GAME EVENT (EVENT IS RAISED WHENEVER A PLAYER COLLIDES WITH A PICKUP)
+    //Method used to pick a random ability from the ability list, currently only the rocket will be chosen (also in the furture can add a timer and play an animation on the ability icon to show a roulette sort of randomising)
+    public void RandomPickupGenerator()
+    {
+        abilityTypeChosen = possibleAbilities[UnityEngine.Random.Range(0, possibleAbilities.Count)];
+
+        if (abilityTypeChosen.abilityName == "NanoBotAbility")
+        {
+            powerup1 = abilityTypeChosen.powerupAbilities[0];
+            powerup2 = abilityTypeChosen.powerupAbilities[1];
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(powerup1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(powerup2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
+        }
+        else if (abilityTypeChosen.abilityName == "ExplosiveAbility")
+        {
+            projectile1 = abilityTypeChosen.projectileAbilities[0];
+            projectile2 = abilityTypeChosen.projectileAbilities[1];
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(projectile1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(projectile2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
+        }
+        else if (abilityTypeChosen.abilityName == "ElectricAbility")
+        {
+            projectile1 = abilityTypeChosen.projectileAbilities[0];
+            projectile2 = abilityTypeChosen.projectileAbilities[1];
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(projectile1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(projectile2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
+        }
+        else if (abilityTypeChosen.abilityName == "ChargeAbility")
+        {
+            powerup1 = abilityTypeChosen.powerupAbilities[0];
+            powerup2 = abilityTypeChosen.powerupAbilities[1];
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(powerup1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(powerup2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
+        }
+        else if (abilityTypeChosen.abilityName == "TeleportAbility")
+        {
+            powerup1 = abilityTypeChosen.powerupAbilities[0];
+            powerup2 = abilityTypeChosen.powerupAbilities[1];
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(powerup1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
+            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(powerup2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
+        }
+
+        abilityUIImage.SetActive(true); //Sets the ability HUD game object to active so the ability can be used
+    }
+    public void AbilityTrigger(float abilityPower)
+    {
+        if (ability.canTriggerAbility && abilityUIImage.activeSelf) //Check if the ability can be fired and if the game object is active in the scene
+        {
+            if (abilityPower == 0)
+            {
+                ability.ButtonTriggered(0); //Trigger ability
+            }
+            else
+            {
+                ability.ButtonTriggered(1); //Trigger ability
+            }
+        }
+    }
+}
--- Assets/Scripts/CarControllers/CarController.cs
+++ Assets/Scripts/CarControllers/CarController.cs
@@ -8,20 +8,9 @@ using XInputDotNetPure;
 public class CarController: MonoBehaviour
 {
     #region Variable Setup
-    public AbilityCoolDown ability; //Reference to the UI ability (needs to be called when a button is pressed)
-    public GameObject abilityUIImage; //Reference to the game object that contains the ability UI (makes sure that it only works when activated, activation when picking up powerup occurs etc)
-
     public ControllerSetup controllerSetup;
 
     public Transform centerMass; //Used as the transform position for the center of mass
-
-    //Creating a list of possible abilites/powerups
-    public List<AbilityHolder> possibleAbilities;
-    public AbilityHolder abilityTypeChosen;
-    public PowerupAbility powerup1;
-    public PowerupAbility powerup2;
-    public ProjectileAbility projectile1;
-    public ProjectileAbility projectile2;
 
     public enum PlayerNumber { p1, p2 }; //Enum setup for setting players, currently only p1 works
     public PlayerNumber thisNumber;
@@ -57,7 +46,6 @@ public class CarController: MonoBehaviour
     {
         controllerSetup = GetComponent<ControllerSetup>();
         rb = GetComponent<Rigidbody>();
-        abilityUIImage.SetActive(false);
         carInfo = GetComponent<CarInfo>();
 
         //Setting a center mass removes colliders from calulation of mass (MEANING WE CAN SET COLLIDERS FREELY NOW!!!)
@@ -91,11 +79,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (controllerSetup.state1.Buttons.RightShoulder == ButtonState.Pressed)
                 {
-                    AbilityTrigger(0);
+                    GetComponent<CarAbility>().AbilityTrigger(0);
                 }
                 if (controllerSetup.state1.Buttons.LeftShoulder == ButtonState.Pressed)
                 {
-                    AbilityTrigger(1);
+                    GetComponent<CarAbility>().AbilityTrigger(1);
                 }
             }
             #endregion
@@ -108,11 +96,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (Input.GetKey(KeyCode.E))
                 {
-                    AbilityTrigger(0);
+                    GetComponent<CarAbility>().AbilityTrigger(0);
                 }
                 if (Input.GetKey(KeyCode.Q))
                 {
-                    AbilityTrigger(1);
+                    GetComponent<CarAbility>().AbilityTrigger(1);
                 }
             }
             #endregion
@@ -131,11 +119,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (controllerSetup.state2.Buttons.RightShoulder == ButtonState.Pressed)
                 {
-                    AbilityTrigger(0);
+                    GetComponent<CarAbility>().AbilityTrigger(0);
                 }
                 if (controllerSetup.state2.Buttons.LeftShoulder == ButtonState.Pressed)
                 {
-                    AbilityTrigger(1);
+                    GetComponent<CarAbility>().AbilityTrigger(1);
                 }
             }
             #endregion
@@ -148,11 +136,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (Input.GetKey(KeyCode.KeypadEnter))
                 {
-                    AbilityTrigger(0);
+                    GetComponent<CarAbility>().AbilityTrigger(0);
                 }
                 if (Input.GetKey(KeyCode.KeypadPeriod))
                 {
-                    AbilityTrigger(1);
+                    GetComponent<CarAbility>().AbilityTrigger(1);
                 }
             }
             #endregion
@@ -267,50 +255,7 @@ public class CarController: MonoBehaviour
         } 
     }
 
-    //THIS METHOD IS CALLED USEING THE PICKUP GAME EVENT (EVENT IS RAISED WHENEVER A PLAYER COLLIDES WITH A PICKUP)
-    //Method used to pick a random ability from the ability list, currently only the rocket will be chosen (also in the furture can add a timer and play an animation on the ability icon to show a roulette sort of randomising)
-    public void RandomPickupGenerator()
-    {
-        abilityTypeChosen = possibleAbilities[UnityEngine.Random.Range(0, possibleAbilities.Count)];
-
-        if (abilityTypeChosen.abilityName == "NanoBotAbility")
-        {
-            powerup1 = abilityTypeChosen.powerupAbilities[0];
-            powerup2 = abilityTypeChosen.powerupAbilities[1];
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(powerup1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(powerup2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
-        }
-        else if (abilityTypeChosen.abilityName == "ExplosiveAbility")
-        {
-            projectile1 = abilityTypeChosen.projectileAbilities[0];
-            projectile2 = abilityTypeChosen.projectileAbilities[1];
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(projectile1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(projectile2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
-        }
-        else if (abilityTypeChosen.abilityName == "ElectricAbility")
-        {
-            projectile1 = abilityTypeChosen.projectileAbilities[0];
-            projectile2 = abilityTypeChosen.projectileAbilities[1];
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(projectile1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(projectile2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
-        }
-        else if (abilityTypeChosen.abilityName == "ChargeAbility")
-        {
-            powerup1 = abilityTypeChosen.powerupAbilities[0];
-            powerup2 = abilityTypeChosen.powerupAbilities[1];
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(powerup1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(powerup2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
-        }
-        else if (abilityTypeChosen.abilityName == "TeleportAbility")
-        {
-            powerup1 = abilityTypeChosen.powerupAbilities[0];
-            powerup2 = abilityTypeChosen.powerupAbilities[1];
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize1(powerup1, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder1);
-            abilityUIImage.GetComponent<AbilityCoolDown>().Initialize2(powerup2, abilityUIImage.GetComponent<AbilityCoolDown>().weaponHolder2);
-        }
-
-        abilityUIImage.SetActive(true); //Sets the ability HUD game object to active so the ability can be used
-    }
+    
 
     #region Movement Methods
     void ForwardMovement(float forward)
@@ -343,19 +288,5 @@ public class CarController: MonoBehaviour
     }
     #endregion
 
-    void AbilityTrigger(float abilityPower)
-    {
-        if (ability.canTriggerAbility && abilityUIImage.activeSelf) //Check if the ability can be fired and if the game object is active in the scene
-        {
-            if (abilityPower == 0)
-            {
-                ability.ButtonTriggered(0); //Trigger ability
-            }
-            else
-            {
-                ability.ButtonTriggered(1); //Trigger ability
-            }
-        }
-    }
     #endregion
 }
