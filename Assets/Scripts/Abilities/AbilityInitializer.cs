0fd94dbb3b058e28e2355a1b5ddcbec75b476ddc
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-05-08 03:03:28 +0100

    region marking and method name changing for quality of life

--- Assets/Scripts/Abilities/AbilityInitializer.cs
+++ Assets/Scripts/Abilities/AbilityInitializer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 //Script that is used as the central point for the ability system (Script is used for UI setup, initalizing each ability and checking if it is ready to be triggered etc)
 public class AbilityInitializer : MonoBehaviour
 {
+    #region Variable Setup
     [SerializeField] bool canTriggerAbility = true; //Bool for checking if the ability can be used (Can add in a cooldown setup with this if we want multiple ability uses in the future etc)
 
     [SerializeField] Ability abilityAttack; //Variable holding the first ability
@@ -17,33 +18,32 @@ public class AbilityInitializer : MonoBehaviour
 
     [SerializeField] List<AbilitySet> possibleAbilitySets; //manually add the list of possible powerups to choose from
     private AbilitySet chosenAbilitySet;
+    #endregion
 
+    #region Ability Selection and Initialization
     public void RandomPickupSelector()
     {
         chosenAbilitySet = possibleAbilitySets[Random.Range(0, possibleAbilitySets.Count)]; //chooses a random ability set from a list of all ability sets available
-        InitializeAbility(chosenAbilitySet);
-        abilityUI.gameObject.SetActive(true);
+        VerifyAbilitySetName(chosenAbilitySet);
     }
-    private void InitializeAbility(AbilitySet abilitySet)
+    private void VerifyAbilitySetName(AbilitySet abilitySet)
     {
         List<string> powerupAbilities = new List<string> { "NanoBotAbility", "ChargeAbility", "TeleportAbility" }; //Creates a list of names that powerup abilities use
         List<string> projectileAbilities = new List<string> { "ExplosiveAbility", "ElectricAbility" };//Creates a list of names that projectile abilities use
 
         if (powerupAbilities.Contains(abilitySet.abilityName))
         {
-            InitializeStyle(abilitySet.powerupAbilities[0], weaponHolderAttack, AbilityDeployModes.DeployStyle.Attack); //Initalize the powerup and set its weapon holder
-            InitializeStyle(abilitySet.powerupAbilities[1], weaponHolderDefense, AbilityDeployModes.DeployStyle.Defense); //Initalize the powerup and set its weapon holder
+            InitialiseAbility(abilitySet.powerupAbilities[0], weaponHolderAttack, AbilityDeployModes.DeployStyle.Attack); //Initalize the powerup and set its weapon holder
+            InitialiseAbility(abilitySet.powerupAbilities[1], weaponHolderDefense, AbilityDeployModes.DeployStyle.Defense); //Initalize the powerup and set its weapon holder
         }
         else if (projectileAbilities.Contains(abilitySet.abilityName))
         {
-            InitializeStyle(abilitySet.projectileAbilities[0], weaponHolderAttack, AbilityDeployModes.DeployStyle.Attack); //Initalize the powerup and set its weapon holder
-            InitializeStyle(abilitySet.projectileAbilities[1], weaponHolderDefense, AbilityDeployModes.DeployStyle.Defense); //Initalize the powerup and set its weapon holder
+            InitialiseAbility(abilitySet.projectileAbilities[0], weaponHolderAttack, AbilityDeployModes.DeployStyle.Attack); //Initalize the powerup and set its weapon holder
+            InitialiseAbility(abilitySet.projectileAbilities[1], weaponHolderDefense, AbilityDeployModes.DeployStyle.Defense); //Initalize the powerup and set its weapon holder
         }
-
     }
-    private void InitializeStyle(Ability selectedAbility, GameObject weaponHolder, AbilityDeployModes.DeployStyle style)
+    private void InitialiseAbility(Ability selectedAbility, GameObject weaponHolder, AbilityDeployModes.DeployStyle style)
     {
-        abilityUI.AssignSprite(selectedAbility.Sprite);
         selectedAbility.Initialize(weaponHolder);
         if (style == AbilityDeployModes.DeployStyle.Attack)
         {
@@ -53,31 +53,39 @@ public class AbilityInitializer : MonoBehaviour
         {
             abilityDefense = selectedAbility;
         }
+        abilityUI.AssignSprite(selectedAbility.Sprite);
+        abilityUI.gameObject.SetActive(true);
         // mark this ability as initialized and ready to deploy
         canTriggerAbility = true;
     }
-    public void TriggerAbility(AbilityDeployModes.DeployStyle style)
+    #endregion
+    #region Ability Triggering and Deployment
+    public void TriggerAbilitySequence(AbilityDeployModes.DeployStyle style)
     {
         if (canTriggerAbility && abilityUI.gameObject.activeSelf) //Check if the ability can be fired and if the game object is active in the scene
         {
             //if the attack style is pressed
             if (style == AbilityDeployModes.DeployStyle.Attack)
             {
-                TriggerAbilitySequence(abilityAttack, style);
+                AbilityDeploy(abilityAttack, style);
             }
             //if the defense style is pressed
             else if (style == AbilityDeployModes.DeployStyle.Defense)
             {
-                TriggerAbilitySequence(abilityDefense, style);
+                AbilityDeploy(abilityDefense, style);
             }
         }
     }
-    private void TriggerAbilitySequence(Ability ability, AbilityDeployModes.DeployStyle deployStyle)
+    private void AbilityDeploy(Ability ability, AbilityDeployModes.DeployStyle deployStyle)
     {
-        abilityUI.PlaySoundEffect(ability.Sound);
         ability.TriggerAbility(deployStyle); //Triggers the ability based on the attack style
+
+        abilityUI.PlaySoundEffect(ability.Sound);
         abilityUI.AssignSprite(null); //removes the icon from the UI image
-        canTriggerAbility = false; //Sets the canTriggerAbility to false
         abilityUI.gameObject.SetActive(false); //makes the active state of the gameObject attached to this script to false (Removes blank UI image and also helps make sure nothing can be activated)
+
+        //mark the ability as used, and stop more triggering events in TriggerAbilitySequence
+        canTriggerAbility = false;
     }
+    #endregion
 }
--- Assets/Scripts/CarControllers/CarController.cs
+++ Assets/Scripts/CarControllers/CarController.cs
@@ -79,11 +79,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (controllerSetup.state1.Buttons.RightShoulder == ButtonState.Pressed)
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Attack);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Attack);
                 }
                 if (controllerSetup.state1.Buttons.LeftShoulder == ButtonState.Pressed)
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Defense);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Defense);
                 }
             }
             #endregion
@@ -96,11 +96,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (Input.GetKey(KeyCode.E))
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Attack);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Attack);
                 }
                 if (Input.GetKey(KeyCode.Q))
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Defense);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Defense);
                 }
             }
             #endregion
@@ -119,11 +119,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (controllerSetup.state2.Buttons.RightShoulder == ButtonState.Pressed)
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Attack);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Attack);
                 }
                 if (controllerSetup.state2.Buttons.LeftShoulder == ButtonState.Pressed)
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Defense);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Defense);
                 }
             }
             #endregion
@@ -136,11 +136,11 @@ public class CarController: MonoBehaviour
                 //Activates the ability
                 if (Input.GetKey(KeyCode.KeypadEnter))
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Attack);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Attack);
                 }
                 if (Input.GetKey(KeyCode.KeypadPeriod))
                 {
-                    GetComponent<AbilityInitializer>().TriggerAbility(AbilityDeployModes.DeployStyle.Defense);
+                    GetComponent<AbilityInitializer>().TriggerAbilitySequence(AbilityDeployModes.DeployStyle.Defense);
                 }
             }
             #endregion
