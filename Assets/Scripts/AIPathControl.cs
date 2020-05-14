a7c7a89e532a0f4359b93d9c0a00ab87e3c19e41
James Home <jamesfoy17>
2020-05-14 23:37:45 +0100

    Comments Added to AI Scripts

--- Assets/Scripts/AIPathControl.cs
+++ Assets/Scripts/AIPathControl.cs
@@ -6,23 +6,22 @@ using UnityEngine;
 public class AIPathControl : MonoBehaviour
 {
     [SerializeField]
-    Transform path;
+    Transform path; //variable for the path parent object that contains all the nodes
 
     [SerializeField]
-    private List<Transform> nodes;
+    private List<Transform> nodes; //Creates a list of transforms
 
-    Vector3 nodeTarget;
+    Vector3 nodeTarget; //Creates a vector3
 
     [SerializeField]
-    int currentNode = 0;
+    int currentNode = 0; //Sets the current node number (can be used to skip nodes in testing but also needed for the AI to work)
 
     Rigidbody rb;
     CarInfo carInfo;
     CarController carController;
 
-    bool isFalling;
-    float angleTarget;
-    float turn;
+    float angleTarget; //Creates a float
+    float turn; //Creates a float
 
     // Start is called before the first frame update
     void Start()
@@ -31,8 +30,9 @@ public class AIPathControl : MonoBehaviour
         carInfo = GetComponent<CarInfo>();
         carController = GetComponent<CarController>();
 
-        path = Path.Instance.transform;
+        path = Path.Instance.transform; //Gets the instance of path
 
+        //if path is not null then create a list and get all children of the path object (works in the same way as the Path script)
         if (path != null)
         {
             Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
@@ -60,32 +60,37 @@ public class AIPathControl : MonoBehaviour
         ApplyRotation();
     }
 
-
+    //Method for applying a forward movement to the AI car
     private void ApplyMovement()
     {
-        carInfo.carStats.speed = Mathf.LerpAngle(carInfo.carStats.speed, carInfo.carStats.maxSpeed, Time.deltaTime / 2);
+        carInfo.carStats.speed = Mathf.LerpAngle(carInfo.carStats.speed, carInfo.carStats.maxSpeed, Time.deltaTime / 2); //Sets the cars speed to gradually increase in the same way as the player cars
 
-        rb.AddForce(transform.forward * carInfo.carStats.speed, ForceMode.Acceleration);
+        rb.AddForce(transform.forward * carInfo.carStats.speed, ForceMode.Acceleration); //Applies a force to push the AI car forward based on the car speed (works in the same way as the player car)
     }
 
+    //Method for finding and applying the correct rotation to the car when going between nodes
     private void ApplyRotation()
     {
-        Quaternion targetRotation = Quaternion.LookRotation(nodes[currentNode].position - transform.position);
+        Quaternion targetRotation = Quaternion.LookRotation(nodes[currentNode].position - transform.position); //targetRotation is the current node - the players position
 
-        nodeTarget = Vector3.zero;
+        nodeTarget = Vector3.zero; //Makes sure nodeTarget is 0
 
+        //if currentNode is equal to the last node
         if (currentNode == nodes.Count - 1)
         {
+            //then the nodeTarget to use is the first in the list (makes sure the car loops the Path)
             nodeTarget = nodes[currentNode - nodes.Count + 1].localPosition;
         }
         else
         {
+            //Otherwise the nodeTarget is the node after the current node
             nodeTarget = nodes[currentNode + 1].localPosition;
         }
 
-        angleTarget = Vector3.Angle(nodes[currentNode].localPosition, nodeTarget);
+        angleTarget = Vector3.Angle(nodes[currentNode].localPosition, nodeTarget); //Finds the angle between the current node position and the target node (used for calcualting how much rotation the car should use)
         Debug.Log(angleTarget);
 
+        //If statements for changing how sharp/quick the car should rotate towards the current node when passing the previous node (makes the car rotate slowly around nodes... the higher the angle the quicker the car needs to turn)
         if (angleTarget < 10f)
         {
             turn = Mathf.LerpAngle(turn, angleTarget, Time.deltaTime / 1000000);
@@ -96,20 +101,25 @@ public class AIPathControl : MonoBehaviour
         }
         else if (angleTarget > 50)
         {
-            turn = Mathf.LerpAngle(turn, angleTarget, Time.deltaTime / 1000);
+            turn = Mathf.LerpAngle(turn, angleTarget, Time.deltaTime / 100);
         }
 
+        //Applies the rotation to the AI car
         rb.MoveRotation(Quaternion.RotateTowards(transform.localRotation, targetRotation, turn));
     }
 
+
+    //Method used to check the distance between the AI cars postion and the current nodes position, if the car gets within a certain distance it changes the current node to the next one in the list
     private void CheckWaypointLength()
     {
         if (Vector3.Distance(transform.position, nodes[currentNode].position) < 20f)
         {
+            //Checks if the current node is the last in the list if so the current node needs to be the first in the list
             if (currentNode == nodes.Count - 1)
             {
                 currentNode = 0;
             }
+            //otherwise increase the current node number
             else
             {
                 currentNode++;
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script used to contain all the information about the nodes and draw them in the Unity Editior
 public class Path : MonoBehaviour
 {
+    //Creates an instace of the path script (will need changing int the future if we plan to have muliple paths)
     public static Path Instance;
     private void Awake()
     {
@@ -18,41 +20,46 @@ public class Path : MonoBehaviour
         }
     }
 
-    public Color lineColor;
+    public Color lineColor; //Varibale for the colour of the line in the editor
 
-    public List<Transform> nodes = new List<Transform>();
+    public List<Transform> nodes = new List<Transform>(); //List of transforms
 
+    //Method that is used to draw the path within the editor whenever the Path object is selected
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = lineColor;
+        Gizmos.color = lineColor; //Sets the colour to the varibale colour
 
-        Transform[] pathTransforms = GetComponentsInChildren<Transform>();
-        nodes = new List<Transform>();
+        Transform[] pathTransforms = GetComponentsInChildren<Transform>(); //Creates a new array of Transforms which are the children of this gameobject
+        nodes = new List<Transform>(); //Makes sure the list is empty
 
+        //Loops through all the transforms found and exludes the transform on the parent object
         for (int i = 0; i < pathTransforms.Length; i++)
         {
             if (pathTransforms[i] != transform)
             {
-                nodes.Add(pathTransforms[i]);
+                nodes.Add(pathTransforms[i]); //Adds every child object to the list
             }
         }
 
+        //loop through the list of nodes and find the previous and current node to draw lines
         for (int i = 0; i < nodes.Count; i++)
         {
-            Vector3 currentNode = nodes[i].position;
-            Vector3 previousNode = Vector3.zero;
+            Vector3 currentNode = nodes[i].position; //Creates a Vector3 which stores the current index of nodes
+            Vector3 previousNode = Vector3.zero; //Creates a Vector3 and makes sure it is set to 0 to begin with
 
+            //If the index is greater then one then the previousNode is the current index - 1
             if (i > 0)
             {
                 previousNode = nodes[i - 1].position;
             }
+            //Else if the current node is 0 or greater then the node list the previous node is the last one in the list (this makes the draw gizmo line loop)
             else if (i == 0 && nodes.Count > 1)
             {
                 previousNode = nodes[nodes.Count - 1].position;
             }
 
-            Gizmos.DrawLine(previousNode, currentNode);
-            Gizmos.DrawWireSphere(currentNode, .5f);
+            Gizmos.DrawLine(previousNode, currentNode); //Draws a line
+            Gizmos.DrawWireSphere(currentNode, .5f); //Draws a sphere on each node
         }
     }
 }
