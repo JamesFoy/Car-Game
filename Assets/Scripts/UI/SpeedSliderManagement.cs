cf5f7745cb8cb02d3a05ece60733984719443643
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-05-17 00:25:48 +0100

    slider update to UI - now actually responds to current speed

---
+++ Assets/Scripts/UI/SpeedSliderManagement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedSliderManagement : MonoBehaviour
+{
+    public List<GameObject> sliderBars;
+    public CarInfo carInfo;
+
+    private void Update()
+    {
+        for (int i = 0; i < sliderBars.Count; i++)
+        {
+            sliderBars[i].SetActive(false);
+        }
+
+        float barValue = carInfo.carStats.maxSpeed / sliderBars.Count;
+        int numberOfBarsToEnable = Mathf.FloorToInt(carInfo.carStats.speed / barValue);
+        Debug.Log(numberOfBarsToEnable);
+
+        for (int i = 0; i < numberOfBarsToEnable; i++)
+        {
+            sliderBars[i].SetActive(true);
+        }
+    }
+}
