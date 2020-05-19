63a97f782f416291ef1850dd27ffa64bcdbf95e1
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-05-19 05:11:24 +0100

    main menu given a quick style update

---
+++ Assets/Scripts/UI/DisplayApplicationVersion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayApplicationVersion : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = "Space Racers v" + Application.version + ", built using Unity " + Application.unityVersion;
+    }
+}
