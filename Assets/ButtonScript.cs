109991010b7f51845c14e31ad31828cccd411260
James Home <jamesfoy17>
2020-05-22 20:14:26 +0100

    Main Menu Update

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonScript : MonoBehaviour, IPointerEnterHandler, ISelectHandler
+{
+    public GameObject selectionArrows;
+
+    public GameObject trackHologram;
+    public GameObject arenaHologram;
+
+    public UnityEvent onHighlight;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onHighlight.Invoke();
+
+        if (this.gameObject.name == "Track Map Button")
+        {
+            selectionArrows.transform.position = this.transform.position;
+            trackHologram.SetActive(true);
+            arenaHologram.SetActive(false);
+        }
+        else if (this.gameObject.name == "Arena Map Button")
+        {
+            selectionArrows.transform.position = this.transform.position;
+            trackHologram.SetActive(false);
+            arenaHologram.SetActive(true);
+        }
+        else if (this.gameObject.name == "Simple Track Button")
+        {
+            selectionArrows.transform.position = this.transform.position;
+            trackHologram.SetActive(false);
+            arenaHologram.SetActive(false);
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+
+    }
+}
