c2b149f28f0284f6816cb8729292dd67132ba960
James Home <jamesfoy17>
2020-05-24 17:16:20 +0100

    Damage Effects for specific damage values

--- Assets/Scripts/DamageEffectBehaviour.cs
+++ Assets/Scripts/DamageEffectBehaviour.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class DamageEffectBehaviour : MonoBehaviour
 {
     public GameObject damageEffectShader;
+    public GameObject damageEffects10;
+    public GameObject damageEffects30;
+    public GameObject damageEffects60;
 
     CarInfo carInfo;
 
@@ -16,6 +19,7 @@ public class DamageEffectBehaviour : MonoBehaviour
         carInfo = GetComponent<CarInfo>();   
     }
 
+    //Saves the health value on the previous frame to a variable
     private void FixedUpdate()
     {
         healthLastFrame = carInfo.carStats.health;
@@ -24,12 +28,41 @@ public class DamageEffectBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //checks if the health value isnt equal to the previous frames value (if it isnt the same the player has taken damage!!!)
         if (carInfo.carStats.health != healthLastFrame)
         {
             StartCoroutine(DamageFlash());
         }
+
+        if (carInfo.carStats.health >= 10)
+        {
+            damageEffects10.SetActive(true);
+        }
+        else
+        {
+            damageEffects10.SetActive(false);
+        }
+
+        if (carInfo.carStats.health >= 30)
+        {
+            damageEffects30.SetActive(true);
+        }
+        else
+        {
+            damageEffects30.SetActive(false);
+        }
+
+        if (carInfo.carStats.health >= 60)
+        {
+            damageEffects60.SetActive(true);
+        }
+        else
+        {
+            damageEffects60.SetActive(false);
+        }
     }
 
+    //Enumerator used to make the damage flash last half a second
     IEnumerator DamageFlash()
     {
         damageEffectShader.SetActive(true);
