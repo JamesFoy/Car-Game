feb846b1fada7947854418874403628238743714
Adam Gwenter <boffinbox@hotmail.co.uk>
2020-05-24 16:33:13 +0100

    more tutorial scripting, stage 4 introduces players to item boxes

---
+++ Assets/Scripts/Tutorial/TutorialState4.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceRacers.TutorialState
+{
+    public class TutorialState4 : TutorialState
+    {
+        [SerializeField] GameObject pickupHolder;
+        private void Start()
+        {
+            AssignPlayerCarInfo();
+            pickupHolder.SetActive(true);
+        }
+        private void Update()
+        {
+        }
+        public void TutorialStateSatisfied()
+        {
+            tutorialConditionSatisfied = true;
+        }
+    }
+}
